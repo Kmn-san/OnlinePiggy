f1e65e2ec7cb7a1ab87c94e939ba4afdc13e5090
Kmn-san <kumenoshouhei0612@gmail.com>
2026-08-02 18:45:51 +0900

    clear stripe

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,22 +5,12 @@ import userRoutes from "./routes/userRoutes.js"
 import accountRoutes from "./routes/accountRoutes.js"
 import transactionRoutes from "./routes/transactionRoutes.js"
 import premiumRoutes from "./routes/premiumRoutes.js"
-import paymentRoute from "./routes/paymentRoutes.js"
+
 import aiRoutes from "./routes/aiRoutes.js"
 
 import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
-
-app.use("/api/payment", (req, res, next) => {
-    console.log(req.originalUrl);
-
-    if (req.originalUrl === "/api/payment/webhook") {
-        express.raw({ type: "application/json" })(req, res, next)
-    } else {
-        express.json()(req, res, next) // parse json for non-webhook routes
-    }
-}, paymentRoute)
 
 app.use(express.json())
 app.use(clerkMiddleware()) //add auth object under the req => req.auth
